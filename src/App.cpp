4854603a4bc20087e71aea6000efb74c26e15d9a
Marie <m.jacquelin21@laposte.net>
2024-05-24 10:22:30 +0200

    Ajout struct graph

--- src/App.cpp
+++ src/App.cpp
@@ -5,6 +5,8 @@
 #include <img/img.hpp>
 
 #include <sstream>
+#include <fstream>
+#include <string>
 
 #include "simpletext.h"
 #include "utils.hpp"
@@ -18,6 +20,7 @@ App::App() : _previousTime(0.0), _viewSize(2.0) {
     
     _texture = loadTexture(test);
 }
+
 
 void App::setup() {
     // Set the clear color to a nice blue
@@ -109,6 +112,59 @@ void App::size_callback(int width, int height) {
         glOrtho(-_viewSize / 2.0f * aspectRatio, _viewSize / 2.0f * aspectRatio, -_viewSize / 2.0f, _viewSize / 2.0f, -1.0f, 1.0f);
     } else {
         glOrtho(-_viewSize / 2.0f, _viewSize / 2.0f, -_viewSize / 2.0f / aspectRatio, _viewSize / 2.0f / aspectRatio, -1.0f, 1.0f);
+    }
+}
+
+
+
+void App::WeightedGraph::add_vertex(int const id){
+    //find
+    auto A = adjacency_list.find(id);
+    if ( A != adjacency_list.end()) {
+        //Ajout du sommet, créer la liste de edges
+        // adjacency_list.insert(std::make_pair<int, std::vector<WeightedGraphEdge>>(id,{}));
+        adjacency_list[id] = {};
+    }
+}
+
+void App::WeightedGraph::add_directed_edge(int const from, int const to, float const weight){
+    //ajout arête et vertex s'il existe pas
+    auto A = adjacency_list.find(from);
+    auto B = adjacency_list.find(to);
+    if ( A == adjacency_list.end()) { add_vertex(from);}
+    if ( B == adjacency_list.end()) { add_vertex(to);}
+    App::WeightedGraphEdge edge {to,weight};
+    adjacency_list[from].push_back(edge);
+}
+
+void App::WeightedGraph::add_undirected_edge(int const from, int const to, float const weight){
+    add_directed_edge(from, to, weight);
+    add_directed_edge(to,from,weight);
+}
+
+App::WeightedGraph App::WeightedGraph::build_from_adjacency_matrix(std::vector<std::vector<float>> const& adjacency_matrix){
+    App::WeightedGraph graphe {};
+    int i{0};
+    while(i<adjacency_matrix.size()){
+        int a{0};
+        while (a<adjacency_matrix[i].size()){
+            if (adjacency_matrix[i][a]!= 0){
+                graphe.add_directed_edge(i,a,adjacency_matrix[i][a]);
+            }
+            a++;
+        }
+        i++;
+    }
+    return graphe;
+}
+
+
+App::WeightedGraph read_map(std::string lien){
+    std::ifstream file(lien);
+    std::string line; 
+    while (std::getline(file, line))
+    {
+        // Process str
     }
 }
 
