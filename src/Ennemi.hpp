a5ed4d3212fe22c6351f20b59dc0de77e5595c14
Marie <m.jacquelin21@laposte.net>
2024-06-13 15:18:35 +0200

    Ajout dijkstra

---
+++ src/Ennemi.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+namespace Graph {
+    struct WeightedGraphEdge {
+        int to {};
+        float weight {1.0f};
+
+        // default ici permet de définit les opérateurs de comparaison membres à membres automatiquement
+        // Cela ne fonction qu'en C++20, si vous n'avez pas accès à cette version je vous donne les implémentations des opérateurs plus bas
+        bool operator==(WeightedGraphEdge const& other) const = default;
+        bool operator!=(WeightedGraphEdge const& other) const = default;
+    };
+
+    struct WeightedGraph {
+        // L'utilisation d'un tableau associatif permet d'avoir une complexité en O(1) pour l'ajout et la recherche d'un sommet.
+        // Cela permet de stocker les sommets dans un ordre quelconque (et pas avoir la contrainte d'avoir des identifiants (entiers) de sommets consécutifs lors de l'ajout de sommets).
+        // Cela permet également de pouvoir utiliser des identifiants de sommets de n'importe quel type (string, char, int, ...) et pas seulement des entiers.
+        std::unordered_map<int, std::vector<WeightedGraphEdge>> adjacency_list {};
+    };
+    std::unordered_map<int, std::pair<int,float>> dijkstra(std::unordered_map<int, std::vector<WeightedGraphEdge>> adjacency_list, int depart, int arrive);
+} // namespace
