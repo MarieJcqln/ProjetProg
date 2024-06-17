6b9389e9c2381310c3e883608f82bc7f63b9c4ae
Laurine <laurine.allemon@gmail.com>
2024-06-17 16:02:28 +0200

    modif affichage bouton

--- src/App.cpp
+++ src/App.cpp
@@ -21,11 +21,11 @@
 App::App() : _previousTime(0.0), _viewSize(2.0)
 {
     // load what needs to be loaded here (for example textures)
-    img::Image map{img::load(make_absolute_path("images/map.png", true), 3, true)};
-
-    //appelfct(map.data(),map.size())
-
-    // map.data();
+    ///////////////mis dans la fonction onclick mouse
+    // img::Image test{img::load(make_absolute_path("images/map.png", true), 3, true)};
+    // _texture = loadTexture(test);
+    //////////////////////////////
+    // test.data();
     //load charge à partir de en haut à gauche
 
     _texture = loadTexture(map);
@@ -90,16 +90,16 @@ void App::render()
     draw_quad_with_texture(_texturecover);
     glPopMatrix();
 
-    TextRenderer.Label("Example of using SimpleText library", _width / 2, 20, SimpleText::CENTER);
     // render exemple quad
     glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
     glBegin(GL_QUADS);
-    glVertex2f(-0.3f, -0.3f);
-    glVertex2f(0.3f, -0.3f);
-    glVertex2f(0.3f, -0.1f);
-    glVertex2f(-0.3f, -0.1f);
+    glVertex2f(-0.3f, -0.25f);
+    glVertex2f(0.3f, -0.25f);
+    glVertex2f(0.3f, -0.15f);
+    glVertex2f(-0.3f, -0.15f);
     glEnd();
-    TextRenderer.Label("JOUER", _width / 2, 20, SimpleText::CENTER);
+    TextRenderer.Label("BITCHARK", _width / 2, _height / 3, SimpleText::CENTER);
+    TextRenderer.Label("JOUER", _width / 2, 3.7 * _height / 6, SimpleText::CENTER);
 
     // Without set precision
     const std::string angle_label_text{"Angle: " + std::to_string(_angle)};
@@ -117,16 +117,34 @@ void App::render()
     TextRenderer.Render();
 }
 
+void App::pause_menu()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.5); // on ajoute un filtre noir transparent
+                                      // std::cout << "Cursor Position at (" << xpos << " : " << ypos << ")" << std::endl;
+    //ADD TEXT
+    TextRenderer.Label("PARTIE EN PAUSE", _width / 2, 3.7 * _height / 6, SimpleText::CENTER);
+}
+
+static const float GL_VIEW_SIZE = 2.;
+void App::conversion(double &xpos, double &ypos)
+{
+    // xpos = (2 * xpos / _height) - 1;
+    // ypos = -((2 * ypos / _height) - 1);
+    xpos = ((xpos - (_width / 2)) * GL_VIEW_SIZE / _height);
+    ypos = -((ypos - (_height / 2)) * GL_VIEW_SIZE / _height);
+}
+
 ///// CODE AJOUTE 3)2)//////
-void App::key_callback(int key, int scancode, int action, int mods)
+void App::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 ////// FIN CODE AJOUTE 3)2///////
 {
     // std::cout << key << std::endl; //pour voir si on capte bien que je touche une touche
     if (key == GLFW_KEY_A && action == GLFW_PRESS)
     { //on met A car en querty A=Q
-        // glfwSetWindowShouldClose(window, GLFW_TRUE);
-        void glfwSetWindowShouldClose(GLFWwindow * window, int value);
-    } //GLFW_TRUE ou 1 fonctionne
+
+        // void glfwSetWindowShouldClose(GLFWwindow * window, int value);
+        glfwSetWindowShouldClose(window, GLFW_TRUE); //GLFW_TRUE ou 1 fonctionne
+    }
     //SI P => pause
     //SI fleche du haut : aller en haut
     //SI fleche du bas : aller en bas
@@ -134,22 +152,18 @@ void App::key_callback(int key, int scancode, int action, int mods)
     //SI fleche de gauche : aller en gauche
 }
 
-void App::pause_menu()
-{ /* 
-    glClearColor(0.0, 0.0, 0.0, 0.5); // on ajoute un filtre noir transparent
-                                      // std::cout << "Cursor Position at (" << xpos << " : " << ypos << ")" << std::endl;
-    //ADD TEXT*/
-}
-void App::mouse_button_callback(int button, int action, int mods)
+void App::mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
 {
-    double xpos, ypos;
-    //getting cursor position
-    // glfwGetCursorPos(GLFWwindow * window, &xpos, &ypos);
-    void glfwGetCursorPos(GLFWwindow * window, double *xpos, double *ypos);
-    //cursor_position_callback
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) // && xpos == 1.0 && ypos == 0.0) //si appuye sur bouton pause
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
     {
-        pause_menu();
+        double xpos, ypos;
+        //getting cursor position
+        glfwGetCursorPos(window, &xpos, &ypos);
+        conversion(xpos, ypos);
+        std::cout << "Cursor Position at (" << _height << ")" << std::endl;
+        std::cout << "Cursor Position at (" << _width << ")" << std::endl;
+        std::cout << "Cursor Position at (" << xpos << " : " << ypos << ")" << std::endl;
+        cursor_position_callback(window, xpos, ypos);
     }
 }
 
@@ -160,13 +174,26 @@ void App::scroll_callback(double /*xoffset*/, double /*yoffset*/)
 {
 }
 
-void App::cursor_position_callback(double xpos, double ypos)
-{ //getting cursor position
-    // glfwGetCursorPos(&xpos, &ypos);
+void App::cursor_position_callback(GLFWwindow *window, double xpos, double ypos)
+{
 
-    // if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-    // {
-    // }
+    //cursor_position_callback
+    if (xpos <= 0.5f && xpos >= 0.4 && ypos <= 0.5f && ypos >= 0.4) // && xpos == 1.0 && ypos == 0.0) //si appuye sur bouton pause
+    {
+        pause_menu();
+    }
+
+    // float red, green, blue;
+    // glGetFloatv(GL_CURRENT_COLOR, &red);       // Récupérer la composante rouge de la couleur actuelle
+    // glGetFloatv(GL_CURRENT_COLOR + 1, &green); // Récupérer la composante verte de la couleur actuelle
+    // glGetFloatv(GL_CURRENT_COLOR + 2, &blue);  // Récupérer la composante bleue de la couleur actuelle
+
+    // if (red == 1.0f && green == 0.0f && blue == 0.0f) // && xpos == 1.0 && ypos == 0.0) //si appuye sur bouton pause
+    if (xpos <= 0.3f && xpos >= -0.3f && ypos <= -0.15f && ypos >= -0.25f)
+    {
+        img::Image test{img::load(make_absolute_path("images/map.png", true), 3, true)};
+        _texture = loadTexture(test);
+    }
 }
 
 void App::size_callback(int width, int height)
